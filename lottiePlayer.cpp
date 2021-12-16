31c44f17865548d69e932313ca6fe7ffaccfeb46
JongKook Kim <d0nzs00n@gmail.com>
2021-12-16 11:33:14 +0900

    image 표시 기능 추가

--- lottiePlayer.cpp
+++ lottiePlayer.cpp
@@ -6,6 +6,8 @@
 #include "cxxopts/cxxopts.hpp"
 #include "w32window.hpp"
 #include <tchar.h>
+#include <filesystem>
+#include <ciso646>
 
 // SFML
 #pragma comment(lib, "winmm")
@@ -33,13 +35,47 @@ LPCTSTR CLASS_NAME = _T("SFML App");
 #define TIMERID_FADEIN 100
 #define TIMERID_FADEOUT 101
 
-int gFadeInValue = 0;
-int gFadeOutValue = 0xff;
-bool gFadeout = false;
-bool gFadeIn = false;
+class Settings {
+public:
+	size_t width;
+	size_t height;
+	string filename;
+	string title;
+	double speed;
+	double alpha;		// 0.0 ~ 1.0
+	bool visible;
+	int fadeInValue;	// 0 ~ 0xff
+	int fadeOutValue;	// 0xff ~ 0
+	bool useFadeIn;		// 프로그램 시작시 fade-in 효과 사용
+	bool useFadeOut;	// 프로그램 종료시 fade-out 효과 사용
+	size_t timeout;		// msec
+	sf::Color backgroundColor;
+
+	Settings():fadeInValue(0), fadeOutValue(0xff), useFadeIn(false), width(0),
+		height(0), useFadeOut(false), speed(0.0), alpha(1.0), visible(true), 
+		timeout(0) {}
+
+	bool initWithCommandline(cxxopts::ParseResult& cmdline);
+
+	bool isValid() const;
+};
+
+Settings settings_;
+
+class Win {
+public:
+	Win(HINSTANCE hInst, LPCSTR className);
+	virtual ~Win();
+
+private:
+	std::string className_;
+	HINSTANCE hInstance_;
+};
+
 
 int main(int argc, char** argv);
 LRESULT CALLBACK onEvent(HWND handle, UINT message, WPARAM wp, LPARAM lp);
+bool isSupportedImageExtension(const std::filesystem::path& extension);
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_
 	LPSTR lpCmdLine, _In_ int nShowCmd) {
@@ -71,81 +107,73 @@ int main(int argc, char** argv) {
 	
 	if (cmdline.count("help")) {		
 		MessageBox(nullptr, options.help().c_str(), "lottie-player", MB_OK | MB_SYSTEMMODAL | MB_TOPMOST);
-		
 		return 0;
 	}
 
-	const size_t w = cmdline["width"].as<size_t>();
-	const size_t h = cmdline["height"].as<size_t>();
-	string filename = cmdline["file"].as<string>();
-	string title = cmdline["title"].as<string>();
-	string hex = cmdline["bgcolor"].as<string>();
-	double speed = cmdline["speed"].as<double>();
-	double alpha = cmdline["alpha"].as<double>();
-	const bool visible = cmdline["hide"].as<bool>()==false;
-	size_t timeout = cmdline["timeout"].as<size_t>();
-	const bool fadein = cmdline["fadein"].as<bool>();
-	const bool fadeout = cmdline["fadeout"].as<bool>();
+	settings_.initWithCommandline(cmdline);
 
-	gFadeout = fadeout;
-	gFadeIn = fadein;
-
-	if (hex.length() < 6 || hex.length() > 8) hex = "ffffffff";
-	else if (hex.length() == 6) hex += "ff";
-
-	uint32_t backColor = (uint32_t)strtoul(hex.c_str(), nullptr, 16);
+	if (not settings_.isValid()) return 0;
 
 	float thickness = 1.0;
-	sf::Vector2f v2((float)w-thickness*2, (float)h-thickness*2);
+	sf::Vector2f v2((float)settings_.width-thickness*2, 
+		(float)settings_.height-thickness*2);
+
 	sf::RectangleShape rectangle(v2);
 	rectangle.setOutlineColor(sf::Color(59, 59, 59));
 	rectangle.setOutlineThickness(2.0);
 	rectangle.setFillColor(sf::Color(0, 0, 0, 0));
 	rectangle.setOrigin(sf::Vector2f(-thickness, -thickness));
 
-	sf::Vector2i grabbedOffset;
-	bool grabbedWindow = false;
 
+	filesystem::path p = settings_.filename;	
+	if (not p.has_extension()) return 0;
 
-	sf::Color backgroundColor(backColor);
-	
-	auto animator = rlottie::Animation::loadFromFile(filename);	
-	auto frameCount = animator->totalFrame();
+	const size_t w = settings_.width;
+	const size_t h = settings_.height;
 
-	double framerate = animator->frameRate() * speed;
-
-
-	const size_t bufferSize = w * h;
-	auto buffer = unique_ptr<uint32_t[]>(new uint32_t[bufferSize]);
-
-	rlottie::Surface surface(buffer.get(), w, h, w * 4);
+	unique_ptr<rlottie::Animation> animator;
+	size_t frameCount = 0;
+	double frameRate = 0.0;
+	sf::Time interval = sf::Time::Zero;
+	size_t bufferSize = 0;
+	unique_ptr<uint32_t[]> buffer;
 
 	sf::Texture texture;
-	texture.create(w, h);	
 	texture.setSmooth(true);
 
+	const bool isImageFile = isSupportedImageExtension(p.extension());
+
+	rlottie::Surface surface;
+	if (isImageFile) {
+		// 이미지 표시
+		frameCount = 1;
+		texture.loadFromFile(settings_.filename);
+	}
+	else {
+		// lottie 표시
+		animator = move(rlottie::Animation::loadFromFile(settings_.filename));
+		frameCount = animator->totalFrame();
+		frameRate = animator->frameRate() * settings_.speed;
+		interval = sf::microseconds((sf::Int64)(1000000.0 / frameRate));
+
+		bufferSize = w * h;
+		buffer = move(unique_ptr<uint32_t[]>(new uint32_t[bufferSize]));
+
+		surface = rlottie::Surface(buffer.get(), w, h, w * 4);
+		texture.create(w, h);
+	}
+
+	if (frameCount == 0) return 0;
+
+
 	sf::Sprite sprite(texture);
+	auto textureSize = texture.getSize();
+	sprite.setScale((float)w/textureSize.x, (float)h / textureSize.y);
 	
 	size_t frameNo = 0;
 
-	sf::Time interval = sf::microseconds(1000000.0/framerate);
-
-	sf::RenderStates renderstates;
+	Win mainWin(gInst, CLASS_NAME);
 	
-	// Define a class for our main window
-	WNDCLASS windowClass;
-	windowClass.style = 0;
-	windowClass.lpfnWndProc = &onEvent;
-	windowClass.cbClsExtra = 0;
-	windowClass.cbWndExtra = 0;
-	windowClass.hInstance = gInst;
-	windowClass.hIcon = nullptr;
-	windowClass.hCursor = nullptr;
-	windowClass.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_BACKGROUND);
-	windowClass.lpszMenuName = nullptr;
-	windowClass.lpszClassName = CLASS_NAME;
-	RegisterClass(&windowClass);
-
 	POINT pos = { 0, };
 	{
 		// Compute position and size
@@ -156,76 +184,46 @@ int main(int argc, char** argv) {
 	}
 
 	DWORD style = WS_POPUP;
-	if (visible) {
+	if (settings_.visible) {
 		style |= WS_VISIBLE;
 	}
 	
 	DWORD exStyle = WS_EX_TOOLWINDOW | WS_EX_LAYERED| WS_EX_TOPMOST;
 
-	//w32window::makeWindowTransparent(hwnd);
-	//w32window::makeWindowToolWindow(hwnd);
-	
 	// Let's create the main window
-	HWND hMainWnd = CreateWindowEx(exStyle, CLASS_NAME, title.c_str(), style, 
+	HWND hMainWnd = CreateWindowEx(exStyle, CLASS_NAME, settings_.title.c_str(), style, 
 		pos.x, pos.y, w, h, nullptr, nullptr, gInst, nullptr);
 
 
-	if (fadein) {
-		if (visible) {
+	if (settings_.useFadeIn) {
+		if (settings_.visible) {
 			w32window::setWindowAlpha(hMainWnd, 0);
 			SetTimer(hMainWnd, TIMERID_FADEIN, 20, nullptr);
 		}
 	}
 	else {
-		w32window::setWindowAlpha(hMainWnd, (BYTE)(255.0 * alpha));
+		w32window::setWindowAlpha(hMainWnd, (BYTE)(255.0 * settings_.alpha));
 	}
 
 	// Let's create two SFML views
 	const DWORD childStyle = WS_CHILD | WS_VISIBLE | WS_CLIPSIBLINGS;
 	HWND hView = CreateWindow(TEXT("STATIC"), nullptr, childStyle, 0, 0, w, h, 
 		hMainWnd, nullptr, gInst, nullptr);
-	sf::RenderWindow lottieWnd(hView, sf::ContextSettings(32, 0, 8));
+	sf::RenderWindow renderWin(hView, sf::ContextSettings(0, 0, 8));
 	
 	// Loop until a WM_QUIT message is received
 	MSG msg;
 	msg.message = static_cast<UINT>(~WM_QUIT);
 
-	DWORD start = GetTickCount();
+	ULONGLONG start = GetTickCount64();
 
-	/*
-	sf::Text text;
-	sf::Font font;
-	if (!font.loadFromFile("C:/Windows/Fonts/malgun.ttf"))
-	{
-		// error...
-	}
-
-	// select the font
-	text.setFont(font); // font is a sf::Font
-
-	// set the string to display
-	text.setString(L"");
-
-	// set the character size
-	text.setCharacterSize(14); // in pixels, not points!
-
-	// set the color
-	text.setFillColor(sf::Color(99,99,99));
-
-	// set the text style
-	//text.setStyle(sf::Text::Bold);
-
-	sf::FloatRect bounds = text.getLocalBounds();
-	text.setPosition(20, 20);
-	//text.setPosition((w-bounds.width)/2, h - (bounds.height + 30) );
-	*/
 	while (msg.message != WM_QUIT) {
 
-		if (timeout > 0) {
-			if (timeout <= (GetTickCount() - start)) {
-				if (gFadeout) {
+		if (settings_.timeout > 0) {
+			if (settings_.timeout  <= (GetTickCount64() - start)) {
+				if (settings_.useFadeOut) {
 					SetTimer(hMainWnd, TIMERID_FADEOUT, 20, nullptr);
-					timeout = 0;
+					settings_.timeout = 0;
 				}
 				else {
 					break;
@@ -239,44 +237,83 @@ int main(int argc, char** argv) {
 			DispatchMessage(&msg);
 		}
 		else {
-			animator->renderSync(frameNo, surface);
-			frameNo++;
-			if (frameNo > frameCount) {
-				frameNo = 0;
+
+			if (animator) {
+				// lottie인 경우
+				animator->renderSync(frameNo, surface);
+				frameNo++;
+				if (frameNo > frameCount) {
+					frameNo = 0;
+				}
+
+				// argb -> rgba
+				uint32_t* data = buffer.get();
+				for (size_t idx = 0; idx < bufferSize; ++idx) {
+					data[idx] = util::argbToRgba(data[idx]);
+				}
+
+				texture.update((sf::Uint8*)buffer.get());
+
+				renderWin.clear(settings_.backgroundColor);
+
+				renderWin.draw(sprite);
+				renderWin.draw(rectangle);
+
+				renderWin.display();
+
+				sf::sleep(interval);
 			}
+			else {
+				// image인 경우
+				if (frameNo < frameCount) {
+					frameNo++;
+					renderWin.clear(settings_.backgroundColor);
 
-			// argb -> rgba
-			uint32_t* data = buffer.get();
-			for (size_t idx = 0; idx < bufferSize; ++idx) {
-				data[idx] = util::argbToRgba(data[idx]);
+					renderWin.draw(sprite);
+					//renderWin.draw(rectangle);
+
+					renderWin.display();
+				}
+				sf::sleep(sf::milliseconds(10));
 			}
-
-			texture.update((sf::Uint8*)buffer.get());
-
-			lottieWnd.clear(backgroundColor);
-
-			lottieWnd.draw(sprite, renderstates);
-			//lottieWnd.draw(text);
-			lottieWnd.draw(rectangle);
 			
-
-			lottieWnd.display();
-
-
-			sf::sleep(interval);
 		}
 	}
 
 	// Close our SFML views before destroying the underlying window
-	lottieWnd.close();
+	renderWin.close();
 
 	// Destroy the main window (all its child controls will be destroyed)
 	DestroyWindow(hMainWnd);
 	
-	// Don't forget to unregister the window class
-	UnregisterClass(CLASS_NAME, gInst);
-
 	return EXIT_SUCCESS;
+}
+
+bool Settings::initWithCommandline(cxxopts::ParseResult& cmdline) {
+	width = cmdline["width"].as<size_t>();
+	height = cmdline["height"].as<size_t>();
+	filename = cmdline["file"].as<string>();
+	title = cmdline["title"].as<string>();
+	string hex = cmdline["bgcolor"].as<string>();
+	speed = cmdline["speed"].as<double>();
+	alpha = cmdline["alpha"].as<double>();
+	visible = cmdline["hide"].as<bool>() == false;
+	timeout = cmdline["timeout"].as<size_t>();
+	useFadeIn = cmdline["fadein"].as<bool>();
+	useFadeOut = cmdline["fadeout"].as<bool>();
+
+	if (hex.length() < 6 || hex.length() > 8) hex = "ffffffff";
+	else if (hex.length() == 6) hex += "ff";
+
+	backgroundColor = sf::Color((uint32_t)strtoul(hex.c_str(), nullptr, 16));
+
+	return true;
+}
+
+bool Settings::isValid() const {
+	if (filename.empty()) return false;
+
+	return true;
 }
 
 LRESULT CALLBACK onEvent(HWND handle, UINT message, WPARAM wp, LPARAM lp) {
@@ -285,18 +322,18 @@ LRESULT CALLBACK onEvent(HWND handle, UINT message, WPARAM wp, LPARAM lp) {
 		// Quit when we close the main window
 	case WM_CLOSE:
 	{
-		if (gFadeout) {
+		if (settings_.useFadeOut) {
 			SetTimer(handle, TIMERID_FADEOUT, 20, nullptr);
 		}
 		else {
 			PostQuitMessage(0);
 		}
 		return 0;
-	}
+	}break;
 	case WM_KEYDOWN: 
 	{
 		if (wp == VK_ESCAPE) {
-			if (gFadeout) {
+			if (settings_.useFadeOut) {
 				SetTimer(handle, TIMERID_FADEOUT, 20, nullptr);
 			}
 			else {
@@ -308,26 +345,26 @@ LRESULT CALLBACK onEvent(HWND handle, UINT message, WPARAM wp, LPARAM lp) {
 	case WM_TIMER:
 	{
 		if (wp == TIMERID_FADEIN) {
-			gFadeInValue += 12;
-			if (gFadeInValue > 0xff) {
-				gFadeInValue = 0xff;
+			settings_.fadeInValue += 12;
+			if (settings_.fadeInValue > 0xff) {
+				settings_.fadeInValue = 0xff;
 			}
-			w32window::setWindowAlpha(handle, gFadeInValue);
+			w32window::setWindowAlpha(handle, settings_.fadeInValue);
 
-			if (gFadeInValue >= 0xff) {
+			if (settings_.fadeInValue >= 0xff) {
 				KillTimer(handle, wp);
-				gFadeInValue = 0;
+				settings_.fadeInValue = 0;
 			}
 		}
 		else if (wp == TIMERID_FADEOUT) {
-			gFadeOutValue -= 12;
-			if (gFadeOutValue < 0) {
-				gFadeOutValue = 0;
+			settings_.fadeOutValue -= 12;
+			if (settings_.fadeOutValue < 0) {
+				settings_.fadeOutValue = 0;
 			}
-			w32window::setWindowAlpha(handle, gFadeOutValue);
+			w32window::setWindowAlpha(handle, settings_.fadeOutValue);
 
-			if (gFadeOutValue <= 0) {
-				gFadeOutValue = 0xff;
+			if (settings_.fadeOutValue <= 0) {
+				settings_.fadeOutValue = 0xff;
 				KillTimer(handle, wp);
 				PostQuitMessage(0);
 			}
@@ -336,7 +373,7 @@ LRESULT CALLBACK onEvent(HWND handle, UINT message, WPARAM wp, LPARAM lp) {
 	case WM_SHOWWINDOW:
 	{
 		if (wp == TRUE) {
-			if (gFadeIn) {
+			if (settings_.useFadeIn) {
 				w32window::setWindowAlpha(handle, 0);
 				SetTimer(handle, TIMERID_FADEIN, 20, nullptr);
 			}
@@ -380,3 +417,72 @@ LRESULT CALLBACK onEvent(HWND handle, UINT message, WPARAM wp, LPARAM lp) {
 	
 	return DefWindowProc(handle, message, wp, lp);
 }
+
+bool isSupportedImageExtension(const std::filesystem::path& extension) {
+	// https://www.sfml-dev.org/documentation/2.5.1/classsf_1_1Image.php
+	// The supported image formats are bmp, png, tga, jpg, gif, psd, hdr and pic.
+	const wstring imgFormats[] = {
+		L".bmp", L".png", L".tga", L".jpg", L".gif", L".psd", L".hdr", L".pic" };
+
+	for (auto& ext : imgFormats) {
+		if (_wcsicmp(ext.c_str(), extension.c_str()) == 0) {
+			return true;
+			break;
+		}
+	}
+	return false;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+Win::Win(HINSTANCE hInst, LPCSTR className):hInstance_(hInst) {
+	className_ = className ? className : "";
+
+
+	// Define a class for our main window
+	WNDCLASS windowClass;
+	windowClass.style = 0;
+	windowClass.lpfnWndProc = &onEvent;
+	windowClass.cbClsExtra = 0;
+	windowClass.cbWndExtra = 0;
+	windowClass.hInstance = hInstance_;
+	windowClass.hIcon = nullptr;
+	windowClass.hCursor = nullptr;
+	windowClass.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_BACKGROUND);
+	windowClass.lpszMenuName = nullptr;
+	windowClass.lpszClassName = className_.c_str();
+	RegisterClass(&windowClass);
+}
+
+Win::~Win() {
+	UnregisterClass(className_.c_str(), hInstance_);
+}
+
+/*
+	SFML Text example.
+
+	sf::Text text;
+	sf::Font font;
+	if (!font.loadFromFile("C:/Windows/Fonts/malgun.ttf"))
+	{
+		// error...
+	}
+
+	// select the font
+	text.setFont(font); // font is a sf::Font
+
+	// set the string to display
+	text.setString(L"");
+
+	// set the character size
+	text.setCharacterSize(14); // in pixels, not points!
+
+	// set the color
+	text.setFillColor(sf::Color(99,99,99));
+
+	// set the text style
+	//text.setStyle(sf::Text::Bold);
+
+	sf::FloatRect bounds = text.getLocalBounds();
+	text.setPosition(20, 20);
+	//text.setPosition((w-bounds.width)/2, h - (bounds.height + 30) );
+*/
